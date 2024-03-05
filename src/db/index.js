bbbd80cb37067cea3e779abfc0626b9169c35cef
Dev Prajapati <devprajapati742@gmail.com>
2024-03-06 00:32:44 +0530

    complted

--- src/db/index.js
+++ src/db/index.js
@@ -5,11 +5,11 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async() => {
     try {
        const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URL}-${DB_NAME}`)
-       console.log(`\n MongoDB connected !! DB HOST : ${connectionInstance.connection.host}`)//basically ye hamne isliya kiya kyoki kabhi kabhi daatabse koi aur server se connect ho jata hai  sliye use host ka connection.host
+       console.log(`\n MongoDB connected !! DB HOST : ${connectionInstance.connection.host}`)
     } catch (error) {
    
         console.log("database connection :: db :: index.js ",error)
-        process.exit(1)// process basically node ka part hai jo ya bata ha ki ye jo applicatio hai kisi currenct processs pe cahlri hai or process uska refernce hai
+        process.exit(1)
     }
 }
 
